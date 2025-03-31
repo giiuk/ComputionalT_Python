ca6a5dba5a06b93a5750ce2fa81c69f5081e09f7
giiuk <giovannalb070607@gmail.com>
2025-03-31 19:00:47 -0300

    Operadores

---
+++ aulas/AULA_13_03.py
@@ -0,0 +1,134 @@
+#AND e OR - >,<,==,!=
+#
+
+'''frase = "Hello World"
+print(frase)
+print(frase)
+print(frase)
+print(frase)
+print("Hello World")
+
+
+palavra_1 = "Olá "
+palavra_2 = "Mundo"
+print(palavra_1 + palavra_2)
+frase = palavra_1 + palavra_2
+print(frase)
+
+
+frase = "Eu "
+print(frase)
+frase = frase + " " + "sou"
+print(frase)
+frase = frase + " " + "a"
+print(frase)
+frase = frase + " " + "Giovanna"
+print(frase)
+
+
+frase = input("Diga a primeira palavra: ")
+print(frase)
+frase = frase + " " + input("Diga a segunda palavra: ")
+print(frase)
+frase = frase + " " + input("Diga a terceira palavra: ")
+print(frase)
+frase = frase + " " + input("Diga a quarta palavra: ")
+print(frase)
+frase = frase + " " + input("Diga a quinta palavra: ")
+print(frase)
+
+
+
+
+a = 2
+b = 3
+print(type(a))
+print(a + b)
+
+soma = a + b
+print(soma)
+
+sub = a - b
+print(sub)
+
+divisao = a/b
+print(divisao)
+
+multi = a*b
+print(multi)
+
+quadrado = a**b
+print(quadrado)
+
+print(f"A soma entre {a} e {b} é {soma}")
+print(f"A subtração entre {a} e {b} é {sub}")
+print(f"A divisão entre {a} e {b} é {divisao}")
+print(f"A multiplicação entre {a} e {b} é {multi}")
+print(f"A potenciação entre {a} e {b} é {quadrado}")
+
+
+
+nome = input("Diga seu nome: ")
+print(f"Olá, {nome}! Bem vindo à calculadora!")
+a = int(input("Digite o primeiro número: "))
+b = int(input("Digite o segundo número: "))
+print(a+b)
+
+
+a= 2
+b= 3
+print(f"O resultado de {a} > {b} é {a>b}")
+print(f"O resultado de {a} != {b} é {a!=b}")
+print(f"O resultado de {a} < {b} é {a!=b}")
+print(f"O resultado de {a} >= {b} é {a>=b}")
+print(f"O resultado de {a} <= {b} é {a<=b}")
+print(f"O resultado de {a} == {b} é {a==b}")
+
+
+
+
+print(True and False)
+print(True and True)
+print (2>3 and 3>2)
+
+
+
+
+a = 2
+b = 3
+
+
+print("Testes com 'and: ")
+print(f"O resultado de {a} > {b} and {a} > {b}, ou seja, {2 > 3} and { 3 > 2} dá {2 > 3 and 3 > 2}")
+print(f"O resultado de {a} != {b} and {a} < {b}, ou seja, {2 != 3} and { 3 < 2} dá {2 != 3 and 3 < 2}")
+print(f"O resultado de {a} > {b} and {a} == {b}, ou seja, {2 > 3} and { 3 == 2} dá {2 > 3 and 3 == 2}")
+print(f"O resultado de {a} < {b} and {a} > {b}, ou seja, {2 < 3} and { 3 > 2} dá {2 < 3 and 3 > 2}")
+
+print("Testes com 'or: ")
+print(f"O resultado de {a} < {b} or {a} > {b}, ou seja, {2 < 3} or { 3 > 2} dá {2 < 3 or 3 > 2}")
+print(f"O resultado de {a} != {b} or {a} < {b}, ou seja, {2 != 3} or { 3 < 2} dá {2 != 3 or 3 < 2}")
+print(f"O resultado de {a} > {b} or {a} == {b}, ou seja, {2 > 3} or { 3 == 2} dá {2 > 3 or 3 == 2}")
+print(f"O resultado de {a} > {b} or {a} > {b}, ou seja, {2 > 3} or { 3 > 2} dá {2 > 3 or 3 > 2}")
+
+
+idade = int(input("Diga sua idade: "))
+if idade >= 18:
+   print('aoba😎🐱‍🐱‍🐉🤳🐱‍👤🐱‍🏍')
+else:
+   print("QUE FEIO 🤢🤢👊")
+
+
+idoso = input("Você é idoso?\n->")
+deficiente = input("Você é deficiente?\n->")
+if idoso == 'sim' or deficiente == 'sim':
+   print("Estaciona ai 🚗")
+else:
+   print("NÃO PODE ESTACIONAR !! Vo conta pra sua mãe 🤬👊")'''
+
+
+'''idoso = input("Você é idoso?\n->")
+carteirinha = input("Você possui a carteirinha de idoso?\n->")
+if idoso == 'sim' and carteirinha == 'sim':
+   print("Estaciona ai 🚗")
+else:
+   print("NÃO PODE ESTACIONAR !!🤡🤠🥺🤓")'''
