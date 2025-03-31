70c4809c770b05265e900779476b856ea6c2d0f4
giiuk <giovannalb070607@gmail.com>
2025-03-31 18:59:31 -0300

    Introdução Python

---
+++ aulas/AULA_06_03.py
@@ -0,0 +1,24 @@
+#Introdução Python
+
+'''frase = "Hello World"
+print(frase)
+print(frase)
+frase = "Olá Mundo"
+
+
+palavra_1 = "Olá "
+palavra_2 = "Mundo!"
+print(palavra_1 + palavra_2)
+frase = palavra_1 + palavra_2
+print(frase)
+
+
+palavra_1 = input("Diga alguma coisa: ")
+palavra_2 = input("Diga outra coisa: ")
+print(palavra_1 +' '+ palavra_2)
+
+
+frase = 'olá '
+print(frase)
+frase = frase + 'mundo'
+print(frase)'''
