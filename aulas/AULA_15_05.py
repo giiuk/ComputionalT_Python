554d2676c286e0dcba15d08eeedc54753960c838
giiuk <giovannalb070607@gmail.com>
2025-05-15 14:24:19 -0300

    For e Listas

--- aulas/AULA_15_05.py
+++ aulas/AULA_15_05.py
@@ -1,60 +1,134 @@
-ano = input("Diga seu ano de nascimento: ")
-while not ano.isnumeric():
-    ano = input("Diga seu ano de nascimento: ")
-ano = int(ano)
+'''vogais = 0
+for char in 'danilo':
+    if char in ['a','e','i','o','u']:
+        vogais += 1
+print(vogais)'''
 
-vinho1 = 'Pérgola'
-vinho2 = 'Sangue de Boi'
-vinho3 = 'Cantinho do Vale'
+'''for i in range(10):
+    print(i)'''
 
-preco1 = 10
-preco2 = 20
-preco3 = 30
+'''for i in range(1,10,3): #ini, fim, passo
+    print(i)'''
 
-qnt1 = 0
-qnt2 = 0
-qnt3 = 0
+'''for i in range (20,10,-2):
+    print(i)'''
 
-idade = 2025 - ano
-if idade < 18:
-    print("Que feiooo, vô contar p sua mãe!!")
-else:
-    endereco = input("Diga seu endereço: ")
-    while True:
-        escolha = input(f"Essas são nossas opções:\n{vinho1} - {preco1}\n"
-                        f"{vinho2} - {preco2}\n{vinho3} - {preco3}\nQual você quer?")
-        while not (escolha == vinho1 or escolha == vinho2 or escolha == vinho3):
-            escolha = input(f"Essas são nossas opções:\n{vinho1} - {preco1}\n"
-                            f"{vinho2} - {preco2}\n{vinho3} - {preco3}\nQual você quer?")
-
-        qnt = input(f"Quantas garrafas de {escolha} você quer?\n->")
-        while not qnt.isnumeric():
-            print("Inválido")
-            qnt = input(f"Quantas garrafas de {escolha} você quer?\n->")
-        qnt = int(qnt)
-        if escolha == vinho1:
-            preco = preco1
-            qnt1 += qnt
-        elif escolha == vinho2:
-            preco = preco2
-            qnt2 += qnt
-        else:
-            preco = preco3
-            qnt3 += qnt
-        valor_total = qnt*preco
-        continuar = input("Você quer continuar comprando? S/N\n->")
-        while not(continuar == 's' or continuar == 'n'):
-            continuar = input("Você quer continuar comprando? S/N\n->")
-        if continuar == 'n':
-            break
-    frete = 10
-    if valor_total >= 500:
-        print("Frete Grátis!")
-        frete = 0
-    print(f"Obrigado por comprar aqui! Você comprou:\n{qnt1} - {vinho1}\n{qnt2} - {vinho2}\n{qnt3} - {vinho3}\nTotalizando R${valor_total:.2f}"
-          f"\ne seu pedido será entregue em {endereco}")
+'''for i in range(1,11):
+    print(f"Tabuada do {i}")
+    for j in range(1,11):
+        print(f"{i}*{j} = {i*j}")
 
 
+i = 1
+while i < 11:
+    j = 1
+    print(f"Tabuada do {i}")
+    while j < 11:
+        print(f"{i}*{j} = {i*j}")
+        j +=1
+    i += 1
+    print()'''
+
+'''lista = [3,True,1.5,'danilo',[]]
+for elem in lista:
+    print(elem)
+
+print() 
+print(lista[0])
+print(lista[1])
+print(lista[2])
+print(lista[3])
+print(lista[4])'''
+
+'''lista = [3,True,1.5,'danilo',[]]
+for elem in lista:
+    print(elem)
+print()
+
+for i in range(len(lista)):
+    print(f"lista[{i}] = {lista[i]}")'''
+
+'''profs = ['Danilo', 'André', 'Gabi','Celso','Yan','Lucas','Luis']
+materias = ['Python','Storytelling','Sw&TX','Matemática','Edge','Front','Web']
+
+for i in range (len(profs)):
+    print(f"O/a prof {profs[i]} dá {materias[i]}")'''
+
+'''alunos = ['Lucas Sena','Rhariel','Sara','Isabella','Lucas Zago']
+notas = [8,8.5,6,4,1]
+
+for i in range (len(alunos)):
+    if notas[i] >= 6:
+        print(f"O/a aluno(a) {alunos[i]} com nota {notas[i]} está aprovado!!")
+    else:
+        print(f"O/a aluno(a) {alunos[i]} com nota {notas[i]} está reprovado!!")'''
+
+'''#Exercicio 1
+pares = 0
+numeros = [9,7,3,5,2,1,8,6,0,4]
+for i in range (len(numeros)):
+    if numeros[i]%2 == 0:
+        pares += 1
+print(f"O número de números pares é: {pares}")
+
+#Exercicio 2
+#V1
+soma = 0
+numeros = [9,7,3,5,2,1,8,6,0,4]
+for num in numeros:
+    soma += num
+print(soma)
+
+#V2
+soma = 0
+for i in range(len(numeros)):
+    soma += numeros[i]
+print(soma)
 
 
+#Exercicio3
+soma = 0
+for i in range(len(numeros)):
+    soma += numeros[i]
+print(soma)
+media = soma/len(numeros)
+print(media)
+'''
 
+'''lista = []
+lista.append(349) #Append adiciona um elemento no final da lista
+print(lista)
+lista.append(67)
+print(lista)
+lista.append(765)
+print(lista)'''
+
+'''lista = []
+for i in range(10):
+    num = input("Digite um numero: ")
+    while not num.isnumeric():
+        num = input("Digite um numero: ")
+    num = int(num)
+    lista.append(num)
+    print(lista)'''
+
+'''
+lista = [7,3,8,5,2,0,9,6,10,4]
+maior = lista[0]
+for num in lista:
+    print(f"Vou testar se {num} > {maior}")
+    if num > maior:
+        print(f"Deu certo, vou trocar {maior} por {num}")
+        maior = num
+print(f"O maior número em {lista} é {maior}")'''
+
+preco = [600, 50, 80, 1000000, 5]
+carros = ['Mustang', 'Up', 'Gol', 'Polinho Turbão Manual', 'Uno']
+maior = preco[0]
+indice_maior = 0
+for i in range(len(preco)):
+    # print(f"Vou testar se {preco[i]} > {maior}")
+    if preco[i] > maior:
+        maior = preco[i]
+        indice_maior = i
+print(f"O carro mais caro é o {carros[indice_maior]}")
